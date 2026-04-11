a9a8a9bd336a28ced20e0f9582c19ddb0828f4b9
Umashankar Kumar <umashankarkumar9572@gmail.com>
2026-04-11 23:52:30 +0530

    my project

--- Listand/src/App.jsx
+++ Listand/src/App.jsx
@@ -3,26 +3,70 @@ import  React from 'react'
 import './App.css'
 
 function App() {
+
+  const[cart , setCart] = useState([]);
+  const items = [
+    {id: 1 , name: "Apple" , price: 1.5},
+    {id: 2 , name: "Banana" , price: 0.8},
+    {id: 3 , name: "orange" , price: 4.8},
+    {id: 4 , name: "grapes" , price: 7.9},
+  ];
+
+  const addToCart = (product) => {
+    setCart([...cart , product]);
+  };
+
+  const removeFromCart = (product) => {
+    setCart(cart.filter((_ , index)  => index != indexToRemove));
+  };
+  return(
+    <div style={{padding: '30px'}}>
+        <h1>My Shopping Cart</h1>
+        {items.map((item)=> (
+          <button key= {item.id} onClick={() => addToCart(item)}>Add{item.name} (${item.price})</button>
+        ))}
+
+        <h2>Your cart({cart.length})</h2>
+        <ul>
+          {cart.map((item, index) => (
+            <li key={index}>
+              {item.name} - ${item.price}
+              <button onClick={()=> removeFromCart(index)}>Remove </button>
+            </li>
+          ))}
+        </ul>
+
+        <h3>Total price: ${cart.reduce((total, item) => total + item.price, 0).toFixed(2)}</h3>
+    </div>
+  )
+
   //const IPL = ['CSk' , 'mumabi' , 'RCB' , 'punjab']
-    const Name = ['uma' , 'shankar' , 'dhiraj' , 'kumar']
+    // const Name = ['uma' , 'shankar' , 'dhiraj' , 'kumar']
 
     
-      const result = Name.map((Name)=> <h1>{Name} </h1>)
-      return(
-      <div className='app'>
-        <h1>{result}</h1>
-         </div>
-    )
+    //   const result = Name.map((Name)=> <h1>{Name} </h1>)
+    //   return(
+    //   <div className='app'>
+    //     <h1>{result}</h1>
+    //      </div>
+    // )
+    // const girls = ['chhoti' , 'moti' , 'gudiya' , 'kajal' , 'Arti']
+    // const map = girls.map((girls) => <h1>{girls}</h1>)
+    // return(
+    //   <div className='app'> 
+    //     <h1>{map}</h1>
+    //   </div>
+    // )
 
-    const phone = ['iphone' , 'apple' , 'realme' , 'samsung' , 'poco' ]
+    // const phone = ['iphone' , 'apple' , 'realme' , 'samsung' , 'poco' ]
 
-    const map = phone.map((phone) => <h1>{phone}</h1>)
+    // const map = phone.map((phone) => <h1>{phone}</h1>)
 
-    return(
-      <div className='app'>
-        <h1>{map}</h1>
-      </div>
-    )
+    // return(
+    //   <div className='app'>
+    //     <h1>{map}</h1>
+    //   </div>
+    // )
 //     const fruits = ['Apple','mango','pineapple','orange','guava' , 'grapes']
 //     return(
 //       <div>
